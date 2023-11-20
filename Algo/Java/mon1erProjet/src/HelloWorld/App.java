ee07758d2b778682111ea13f2eb87187ef3c1500
LEMMEL Lucas <LLemmel@arfp.asso.fr>
2023-11-20 16:18:33 +0100

    exo d'ago + java

---
+++ Algo/Java/mon1erProjet/src/HelloWorld/App.java
@@ -0,0 +1,40 @@
+package HelloWorld;
+
+public class App {
+
+	public static void main(String[] args) {
+		/*
+		 *Exemple 
+		 * de
+		 * code
+		 * Java
+		 */
+		// Declaration d'u variable de type chaine de caractère
+		String maVariable;
+		
+		// Declaration d'une variable de type entier
+		int nombre;
+		nombre = 5;
+		float nombre2;
+		nombre2 = 2.5f;
+		double nombre3;
+		nombre3 = 3.25;
+		long nombre4;
+		nombre4 = 12254;
+		
+		int resultat = 3+2;
+		
+		// declaration d'une variable de type booléan
+		boolean vraiOuFaux;
+		
+		vraiOuFaux = false;
+		
+		
+		maVariable = "Hello ";
+		
+		System.out.println(maVariable + "World ");
+		
+
+	}
+
+}
